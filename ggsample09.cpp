8018641defc803107aeda19cb22ca07c617fff21
tokoi <tokoi@wakayama-u.ac.jp>
2021-07-16 00:04:27 +0900

    move Window class into GgApp.h and remove Window.h, and update gg.h, gg.cpp

--- ggsample09.cpp
+++ ggsample09.cpp
@@ -1,5 +1,7 @@
-﻿// ウィンドウ関連の処理
-#include "Window.h"
+﻿//
+// ゲームグラフィックス特論宿題アプリケーション
+//
+#include "GgApp.h"
 
 // シェーダー関連の処理
 #include "shader.h"
@@ -8,36 +10,36 @@
 #include <cmath>
 
 // アニメーションの周期（秒）
-const double cycle(5.0);
+constexpr auto cycle{ 5.0 };
 
 // 球のデータの分割数
-const int slices(64);
-const int stacks(32);
+constexpr auto slices{ 64 };
+constexpr auto stacks{ 32 };
 
 // 球のデータの頂点数と面数
-const int vertices((slices + 1) * (stacks + 1));
-const int faces(slices * stacks * 2);
+constexpr auto vertices{ (slices + 1) * (stacks + 1) };
+constexpr auto faces{ slices * stacks * 2 };
 
 //
 // 球のデータの作成
 //
 static void makeSphere(float radius, int slices, int stacks,
-  GLfloat (*pv)[3], GLfloat (*nv)[3], GLfloat (*tv)[2], GLuint (*f)[3])
+  GLfloat(*pv)[3], GLfloat(*nv)[3], GLfloat(*tv)[2], GLuint(*f)[3])
 {
   // 頂点の位置とテクスチャ座標を求める
   for (int k = 0, j = 0; j <= stacks; ++j)
   {
-    const float t(static_cast<float>(j) / static_cast<float>(stacks));
-    const float ph(3.141593f * t);
-    const float y(cos(ph));
-    const float r(sin(ph));
+    const auto t{ static_cast<float>(j) / static_cast<float>(stacks) };
+    const auto ph{ 3.141593f * t };
+    const auto y{ cos(ph) };
+    const auto r{ sin(ph) };
 
     for (int i = 0; i <= slices; ++i)
     {
-      const float s(static_cast<float>(i) / static_cast<float>(slices));
-      const float th(-2.0f * 3.141593f * s);
-      const float x(r * cos(th));
-      const float z(r * sin(th));
+      const auto s{ static_cast<float>(i) / static_cast<float>(slices) };
+      const auto th{ -2.0f * 3.141593f * s };
+      const auto x{ r * cos(th) };
+      const auto z{ r * sin(th) };
 
       // 頂点の座標値
       pv[k][0] = x * radius;
@@ -62,7 +64,7 @@ static void makeSphere(float radius, int slices, int stacks,
   {
     for (int i = 0; i < slices; ++i)
     {
-      const int count((slices + 1) * j + i);
+      const auto count{ (slices + 1) * j + i };
 
       // 上半分の三角形
       f[k][0] = count;
@@ -80,12 +82,12 @@ static void makeSphere(float radius, int slices, int stacks,
 }
 
 //
-// アプリケーションの実行
+// アプリケーション本体
 //
-void app()
+int GgApp::main(int argc, const char* const* argv)
 {
   // ウィンドウを作成する
-  Window window("ggsample09");
+  Window window{ "ggsample09" };
 
   // 背景色を指定する
   glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
@@ -95,21 +97,21 @@ void app()
   glEnable(GL_CULL_FACE);
 
   // プログラムオブジェクトの作成
-  const GLuint program(loadProgram("ggsample09.vert", "pv", "ggsample09.frag", "fc"));
+  const auto program{ loadProgram("ggsample09.vert", "pv", "ggsample09.frag", "fc") };
 
   // in (attribute) 変数のインデックスの検索（見つからなければ -1）
-  const GLint nvLoc(glGetAttribLocation(program, "nv"));
-  const GLint tvLoc(glGetAttribLocation(program, "tv"));
+  const auto nvLoc{ glGetAttribLocation(program, "nv") };
+  const auto tvLoc{ glGetAttribLocation(program, "tv") };
 
   // uniform 変数のインデックスの検索（見つからなければ -1）
-  const GLint mwLoc(glGetUniformLocation(program, "mw"));
-  const GLint mcLoc(glGetUniformLocation(program, "mc"));
-  const GLint mgLoc(glGetUniformLocation(program, "mg"));
-  const GLint colorLoc(glGetUniformLocation(program, "color"));
-  const GLint normalLoc(glGetUniformLocation(program, "normal"));
+  const auto mwLoc{ glGetUniformLocation(program, "mw") };
+  const auto mcLoc{ glGetUniformLocation(program, "mc") };
+  const auto mgLoc{ glGetUniformLocation(program, "mg") };
+  const auto colorLoc{ glGetUniformLocation(program, "color") };
+  const auto normalLoc{ glGetUniformLocation(program, "normal") };
 
   // ビュー変換行列を mv に求める
-  const GgMatrix mv(ggLookat(0.0f, 0.0f, 5.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f));
+  const auto mv(ggLookat(0.0f, 0.0f, 5.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f));
 
   // 図形データの作成
   GLfloat pv[vertices][3];
@@ -156,10 +158,10 @@ void app()
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof face, face, GL_STATIC_DRAW);
 
   // 一つ目のテクスチャ (カラーマップを読み込む)
-  const GLuint color(ggLoadImage("color.tga"));
+  const GLuint color{ ggLoadImage("color.tga") };
 
   // 二つ目のテクスチャ (高さマップを読み込んで法線マップを作る)
-  const GLuint normal(ggLoadHeight("height.tga", 10.0f));
+  const GLuint normal{ ggLoadHeight("height.tga", 10.0f) };
 
   // 経過時間のリセット
   glfwSetTime(0.0);
@@ -174,19 +176,19 @@ void app()
     glUseProgram(program);
 
     // 時刻の計測
-    const float t(static_cast<float>(fmod(glfwGetTime(), cycle) / cycle));
+    const auto t{ static_cast<float>(fmod(glfwGetTime(), cycle) / cycle) };
 
     // モデルビュー変換行列 (時刻 t にもとづく回転アニメーション)
-    const GgMatrix mw(mv.rotateY(12.56637f * t));
+    const auto mw{ mv.rotateY(12.56637f * t) };
 
     // 法線変換行列
-    const GgMatrix mg(mw.normal());
+    const auto mg{ mw.normal() };
 
     // 投影変換行列
-    const GgMatrix mp(ggPerspective(0.5f, window.getAspect(), 1.0f, 15.0f));
+    const auto mp{ ggPerspective(0.5f, window.getAspect(), 1.0f, 15.0f) };
 
     // モデルビュー・投影変換
-    const GgMatrix mc(mp * mw);
+    const auto mc{ mp * mw };
 
     // uniform 変数を設定する
     glUniformMatrix4fv(mwLoc, 1, GL_FALSE, mw.get());
@@ -216,4 +218,6 @@ void app()
     // カラーバッファを入れ替えてイベントを取り出す
     window.swapBuffers();
   }
+
+  return 0;
 }
